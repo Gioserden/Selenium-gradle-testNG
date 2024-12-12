ce6aa4651bc8105a2bc6bf65a39ed101ac18cee0
Sergio <sergio.denn.navarro@gmail.com>
2024-12-11 20:08:11 -0400

    v 0.15

--- src/test/java/com/automation/tests/ConfigTest.java
+++ src/test/java/com/automation/tests/ConfigTest.java
@@ -9,6 +9,7 @@ public class ConfigTest extends TestBase {
 
     @Test
     public void testConfig() {
+        System.out.println("Ruta del ChromeDriver: " + System.getProperty("webdriver.chrome.driver"));
         setup();
         driver.get(ConfigReader.get("base.url"));
         System.out.println("Página cargada: " + driver.getCurrentUrl());
